26f366a9c81667695ee804bcfc07e32eb2a982ac
tonyrealzy <umehobiarinze2@gmail.com>
2026-03-02 20:05:24 +0100

    fix: Made a fix for the Swagger UI view.

--- app/swagger/page.tsx
+++ app/swagger/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { EnvConfig } from "@/config/env";
+// import { EnvConfig } from "@/config/env";
 import "swagger-ui-react/swagger-ui.css";
 
 const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
 
 export default function ApiDocs() {
-  if (EnvConfig.nodeEnv !== "development") {
-    return <SwaggerUI url="/api/docs" />;
-  } else {
-    return <SwaggerUI url="/swagger.json" />;
-  }
+  return <SwaggerUI url="/swagger.json" />;
+
+  // if (EnvConfig.nodeEnv !== "development") {
+  //   return <SwaggerUI url="/api/docs" />;
+  // } else {
+  //   return <SwaggerUI url="/swagger.json" />;
+  // }
 }
